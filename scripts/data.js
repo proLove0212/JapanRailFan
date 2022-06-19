a2a211840a1b525ab277cee65b98b97cfb54f2d7
PROLOVE <proLove0212@gmail.com>
2022-06-20 00:43:54 +0800

    line image gallery & line rolling stock

--- scripts/data.js
+++ scripts/data.js
@@ -8,7 +8,11 @@ const lines = {
         description: "The Yamanote Line (Japanese: 山手線, Yamanote-sen) is the loop service in Tokyo, Japan, operated by East Japan Railway Company (JR East). It is one of Tokyo's busiest and most important lines, connecting most of Tokyo's major stations and urban centres, including Marunouchi, the Yurakucho/Ginza area, Shinagawa, Shibuya, Shinjuku, Ikebukuro, and Ueno, with all but two of its 30 stations connecting to other railway or underground (subway) lines. ",
         color: "9acd32",
         images: [
-            "./imgs/e235-1.jpg",
+            "./imgs/yamanoteLine-1.jpg",
+            "./imgs/yamanoteLine-2.jpg",
+            "./imgs/yamanoteLine-3.jpg",
+            "./imgs/yamanoteLine-4.jpg",
+            "./imgs/yamanoteLine-5.jpg",
         ],
         operator: 1,
         open: 1885,
@@ -23,7 +27,11 @@ const lines = {
         description: "The Chuo-Sobu Line (Japanese: 中央・総武緩行線, Hepburn: Chuo-Sobu-kanko-sen) is a railway line that runs through Tokyo and Chiba Prefecture, Japan. Part of the East Japan Railway Company (JR East) network, the line operates on separate tracks along the right-of-way of the Chuo Main Line (Chuo Line (Rapid)) and Sobu Main Line (Sobu Line (Rapid)), providing service between Mitaka Station in the cities of Mitaka and Musashino and Chiba Station in Chiba.",
         color: "FFD400",
         images: [
-            "./imgs/e235-1.jpg",
+            "./imgs/sobuLine-1.jpg",
+            "./imgs/sobuLine-2.jpg",
+            "./imgs/sobuLine-3.jpg",
+            "./imgs/sobuLine-4.jpg",
+            "./imgs/sobuLine-5.jpg",
         ],
         operator: 1,
         open: 1932,
@@ -44,8 +52,8 @@ const operators = {
 
 const trains = {
     1: {
-        name: "E235-0 Series",
-        description: "The E235-0 Series is a high-capacity train that operates on the Yamanote Line. The train consists of 11 cars and has a capacity of up to 1628 passengers. Cars no. 7 and 10 are configured with 6 doors on each side for quicker boarding and alighting.",
+        name: "E235 Series",
+        description: "The E235 Series is a high-capacity train that operates on the Yamanote Line. The train consists of 11 cars and has a capacity of up to 1628 passengers. Cars no. 7 and 10 are configured with 6 doors on each side for quicker boarding and alighting.",
         built: "2015 - 2019",
         images: [
             "./imgs/e235-1.jpg",
@@ -56,8 +64,8 @@ const trains = {
         capacity: 1628
     },
     2: {
-        name: "E231-500 Series",
-        description: "The E231-500 Series is a high-capacity train that operates on the Chou-Sobu Line and formerly the Yamanote Line. The train consists of 10 cars (Chuo-Sobu Line) or 11 cars (Yamanote Line). Cars no. 7 and 10 are configured with 6 doors on each side for quicker boarding and alighting. (Only for Yamanote Line)",
+        name: "E231 Series",
+        description: "The E231 Series is a high-capacity train that operates on the Chou-Sobu Line and formerly the Yamanote Line. The train consists of 10 cars (Chuo-Sobu Line) or 11 cars (Yamanote Line). Cars no. 7 and 10 are configured with 6 doors on each side for quicker boarding and alighting. (Only for Yamanote Line)",
         built: "2002 - 2005",
         images: [
             "./imgs/e231-1.jpg",
--- scripts/line.js
+++ scripts/line.js
@@ -2,6 +2,9 @@
 
 var id = urlParams.get("line")
 var trainHtml = ""
+var trainContent = document.getElementById("line-trains")
+var carouselGalleryHtml = ""
+var galleryImgsHtml = ""
 
 if (id == null) {
     window.location.href = "./lines.html";
@@ -20,3 +23,39 @@ document.getElementById("line-length").innerHTML = line.length + " km"
 document.getElementById("line-track").innerHTML = line.track + " mm"
 document.getElementById("line-speed").innerHTML = line.speed + " km/h"
 document.getElementById("line-desc").innerHTML = line.description
+
+// Get rolling stock information
+for (const [trainKey, trainValue] of Object.entries(trains)) {
+    if (trainValue.line.includes(id)) {
+        trainHtml += '<div class="col-md-6 col-lg-4 mb-3" id="train-' + trainKey + '"><a href="./train.html?train=' + trainKey + '"><div class="card h-100 h-shadow"><img class="card-img-top" src="' + trainValue.images[0] + '"><div class="card-body d-flex align-items-center"><h5 class="flex-fill m-0">' + trainValue.name + '</h5><span class="material-symbols-sharp white">chevron_right</span></div></div></a></div>'
+    }
+}
+
+// Set rolling stock information
+trainContent.innerHTML = trainHtml
+
+// Todo: Create a function in common.js to shorten code for carousel function
+// Set Carousel for image gallery
+line.images.forEach(setGallery)
+function setGallery(path, index) {
+    carouselGalleryHtml += index == 0 ? '<div class="carousel-item active"><img class="d-block w-100 carousel-gallery-img" src="' + path + '"></div>' : '<div class="carousel-item"><img class="d-block w-100 carousel-gallery-img" src="' + path + '"></div>'
+    galleryImgsHtml += index == 0 ? '<div class="col-4 mb-3"><img id="line-gallery-img-' + index + '" class="gallery-img w-100 h-shadow active" src="' + path + '" onclick="changeGallerySlide(' + index + ')"></div>' : '<div class="col-4 mb-3"><img id="line-gallery-img-' + index + '" class="gallery-img w-100 h-shadow" src="' + path + '" onclick="changeGallerySlide(' + index + ')"></div>'
+}
+
+document.getElementById("carousel-line-gallery-imgs").innerHTML = carouselGalleryHtml
+document.getElementById("line-gallery-imgs").innerHTML = galleryImgsHtml
+
+
+// Carousel functionality
+function changeGallerySlide(slide) {
+    $('#carousel-line-gallery').carousel(slide)
+}
+
+$('#carousel-line-gallery').on('slid.bs.carousel', function (properties) {
+    // change picture size based on the active slide
+    var fromImgElement = document.getElementById("line-gallery-img-" + properties.from)
+    var toImgElement = document.getElementById("line-gallery-img-" + properties.to)
+
+    fromImgElement.classList.remove("active")
+    toImgElement.classList.add("active")
+})
