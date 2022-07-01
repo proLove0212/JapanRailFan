28aa7c7f2a7d72b8b7756be80f06f2acbf430b07
PROLOVE <proLove0212@gmail.com>
2022-07-01 09:17:55 +0800

    Add valid line section to ticket info

--- scripts/ticket.js
+++ scripts/ticket.js
@@ -4,6 +4,7 @@ var id = urlParams.get("ticket")
 var priceTable = document.getElementById("ticket-prices");
 
 var pricesHtml = ""
+var linesHtml = ""
 
 if (id == null) {
     window.location.href = "./lines.html";
@@ -23,8 +24,19 @@ ticket.valid.forEach(function(item, index) {
     priceCell.innerHTML = "¥" + price
 });
 
+for (const [lineKey, value] of Object.entries(lines)) {
+    if (ticket.operators.includes(value.operator)) {
+        var lineName = value.name
+        var length = value.length
+        var color = value.color
+
+        linesHtml += '<div class="col-md-6 col-lg-4 mb-3"><a href="./line.html?line=' + lineKey + '"><div class="card h-shadow"><div class="w-100 d-flex justify-content-center align-items-center" style="background-color:#' + color + ';"><span class="material-symbols-sharp category-icon my-5">route</span></div><div class="card-body d-flex align-items-center"><div class="flex-fill"><h5 class="card-title m-0">' + lineName + '</h5><small class="white">' + length + ' km</small></div><span class="material-symbols-sharp white">chevron_right</span></div></div></a></div>'
+    }
+}
+
 //Set the info panels
 document.title = ticket.name + " - JapanRailFan"
 document.getElementById("ticket-name").innerHTML = ticket.name
 document.getElementById("ticket-desc").innerHTML = ticket.description
 document.getElementById("ticket-consecutive").innerHTML = ticket.vaildConsecutive ? "Consecutive Days" : "Any Day"
+document.getElementById("ticket-vaild-lines").innerHTML = linesHtml
